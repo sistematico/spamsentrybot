de5a87602fa9b20d58cd6b9f92b002ac0341e026
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-02-11 17:12:01 -0400

    Autocommit por GitHub: alterações: add 'public/bot.php'

--- public/bot.php
+++ public/bot.php
@@ -35,12 +35,15 @@ function processMessage($message)
                 apiRequest("sendMessage", array('chat_id' => $chat_id, "reply_to_message_id" => $message_id, "text" => "Tomei a liberdade de apagar esta mensagem.\n\nID: ${reply_id}\n\nAdmin: ${user_id}${user_id2}\n\nRole: ${role}\n\nUsuário original: $originalUsername"));
                 apiRequest("deleteMessage", array('chat_id' => $chat_id, "message_id" => $reply_id));                
                 break;
-            case 'Debug':
+            case (strpos($text, "/debug") === 0):
                 if (isset($originalUsername)) {
                     apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => "Username: $originalUsername"));
                 } else {
                     apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => "Username: $username"));
                 }
+                break;
+            case (strpos($text, "/json") === 0):
+                apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => "DUMP: " var_dump($message)));
                 break;
             default:
                 break;
