8e8a916b40d0f364503cd72c71ea8648b0937869
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-02-11 16:39:01 -0400

    Autocommit por GitHub: alterações: add 'public/bot.php'

--- public/bot.php
+++ public/bot.php
@@ -11,6 +11,7 @@ function processMessage($message)
 {
     // process incoming message
     $message_id = $message['message_id'];
+    $reply_id = $message['reply_to_message_id'];
     $chat_id = $message['chat']['id'];
     $username = '@' . $message['from']['username'];
     $originalUsername = '@' . $message['reply_to_message']['from']['username'];
@@ -25,7 +26,9 @@ function processMessage($message)
 
             case (strpos($text, "/del") === 0):
                 apiRequest("sendChatAction", array('chat_id' => $chat_id, 'action' => 'typing'));
-                apiRequest("sendMessage", array('chat_id' => $chat_id, "reply_to_message_id" => $message_id, "text" => "Tomei a liberdade de apagar esta mesnagem..."));
+                apiRequest("sendMessage", array('chat_id' => $chat_id, "reply_to_message_id" => $message_id, "text" => "Tomei a liberdade de apagar esta mensagem ID: ${reply_id}"));
+                //apiRequest("deleteMessage", array('chat_id' => $chat_id, "message_id" => $message_id, "text" => "Tomei a liberdade de apagar esta mesnagem..."));
+                
                 //apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => "Olá {$username}{$username2}!\n\nEm que posso te ajudar!?"));
                 break;
 
