f5ef4afda6ae87cb0762bb2bc8c13852066d4df3
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-02-12 15:28:02 -0400

    Autocommit por GitHub: alterações: add 'lib/log.php'

--- lib/log.php
+++ lib/log.php
@@ -4,17 +4,26 @@ function readLog()
 {
     $linecount = 0;
     $handle = fopen(LOGPATH, "r");
-    while(!feof($handle)){
-        $line = fgets($handle);
-        $linecount++;
-    }                
+    while (!feof($handle)) {
+        $line = fgets($handle, 4096);
+        $linecount = $linecount + substr_count($line, PHP_EOL);
+    }
     fclose($handle);
+
+
+    // $linecount = 0;
+    // $handle = fopen(LOGPATH, "r");
+    // while (!feof($handle)) {
+    //     $line = fgets($handle);
+    //     $linecount++;
+    // }
+    // fclose($handle);
     $linhas = $linecount;
 
     // Lê os logs
     $logfile = fopen(LOGPATH, "r") or die("Unable to open file!");
-    $log = fread($logfile,filesize(LOGPATH));
+    $log = fread($logfile, filesize(LOGPATH));
     fclose($logfile);
 
-    return array('log'=>$log, 'linhas'=> $linhas);
+    return array('log' => $log, 'linhas' => $linhas);
 }
