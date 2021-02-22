d4ad81dde9438f20df5ad5571811a458c5b083e2
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-02-22 16:20:01 -0400

    Autocommit por GitHub: alterações: add 'public/index.php' add 'public/assets/js/jquery.min.js' add 'public/assets/js/tether.js'

--- public/index.php
+++ public/index.php
@@ -15,24 +15,12 @@
   <link rel="stylesheet" type="text/css" href="assets/css/main.css">
   <!-- Vendors Scripts -->
   <!-- v1.0.0 -->
-  <script src="js/lib/jquery.min.js"></script>
-  <script src="js/lib/tether.js"></script>
-  <script src="js/lib/bootstrap.min.js"></script>
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/tether.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.4/js/bootstrap-select.min.js"></script>
 
-  <script src="js/main.js"></script>
-  <script>
-    (function (i, s, o, g, r, a, m) {
-      i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
-        (i[r].q = i[r].q || []).push(arguments)
-      }, i[r].l = 1 * new Date(); a = s.createElement(o),
-        m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
-    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-
-    ga('create', 'UA-101624591-1', 'auto');
-    ga('send', 'pageview');
-
-  </script>
+  <script src="assets/js/main.js"></script>
 </head>
 
 <body class="theme-default landing-page-body">
