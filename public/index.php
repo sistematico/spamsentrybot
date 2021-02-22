caa9db7051ef349dc5022988ba76974ecce16c98
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-02-22 16:10:01 -0400

    Autocommit por GitHub: alterações: add 'public/index.php'

---
+++ public/index.php
@@ -0,0 +1,247 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <title>SpamSentryBot</title>
+  <link rel="shortcut icon" type="image/png" href="/images/favicon.ico" />
+  <link rel="stylesheet" type="text/css" href="css/vendors/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.4/css/bootstrap-select.css">
+  <link rel="stylesheet" href="css/pages/landing-page.css">
+  <link rel="stylesheet" type="text/css" href="css/main.css">
+  <!-- Vendors Scripts -->
+  <!-- v1.0.0 -->
+  <script src="js/lib/jquery.min.js"></script>
+  <script src="js/lib/tether.js"></script>
+  <script src="js/lib/bootstrap.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.4/js/bootstrap-select.min.js"></script>
+
+  <script src="js/main.js"></script>
+  <script>
+    (function (i, s, o, g, r, a, m) {
+      i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments)
+      }, i[r].l = 1 * new Date(); a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-101624591-1', 'auto');
+    ga('send', 'pageview');
+
+  </script>
+</head>
+
+<body class="theme-default landing-page-body">
+
+  <section class="page-content">
+    <div class="page-content-inner">
+
+      <!-- Register Page -->
+      <div class="single-page-block-header">
+        <div class="row">
+          <div class="col-md-4 col-lg-3">
+            <div class="logo">
+              <a href="javascript: history.back();">
+                <img src="images/web-logo-inverse-02.svg" alt="Kappamon" />
+              </a>
+            </div>
+          </div>
+          <div class="col-md-5 col-lg-4 col-md-offset-3 col-lg-offset-5 col-sm-12">
+            <div class="single-page-block-header-menu margin-top-15">
+              <ul class="list-unstyled list-inline height-50 social-flexbox">
+                <li><a href="https://www.youtube.com/channel/UCoEOWLSaSYVJszVV6J6RhNQ" target="_blank"><i class="icmn-youtube"></i></a></li>
+                <li><a href="https://discord.gg/6nfKQEU"><i class="icon-discord"></i></a></li>
+                <li><a href="https://twitter.com/Kappamon_"><i class="icmn-twitter"></i></a></li>
+                <div class="line">
+                </div>
+                <div id="termsofuse" class="height-50 vertical-align">
+                  <a class="vertical-align" style="font-size: 0.9rem" href="#" data-toggle="modal" data-target="#termsOfUse"
+                    aria-hidden="true">Terms of Use</a>
+                </div>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="landing-page-block">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <img class="center-block landing-page-image" src="images/kappamon_graphic_transparent.svg" />
+            <h1 class="landing-page-header">Your Very Own Virtual Stream Pet</h1>
+            <h5 class="landing-page-subtext">
+              Auto greets viewers, dances to !song, and many more fun interactions with custom commands!
+            </h5>
+            <div class="row">
+              <div class="col-lg-12 text-center">
+                <button class="btn btn-icon btn-danger vertical-align btn-flex margin-bottom-10" data-toggle="modal"
+                  data-target="#promoVideo" aria-hidden="true">
+                  <i class="icmn-youtube margin-right-10 vertical-align-middle" style="font-size: 1.5rem"></i>
+                  <span class="vertical-align-middle" style="font-size: 1.25rem">Watch Video</span>
+                </button>
+                <a href="/login/twitch" class="btn btn-icon btn-purple vertical-align btn-flex margin-bottom-10">
+                  <i class="icmn-twitch margin-right-10 vertical-align-middle" style="font-size: 1.5rem"></i>
+                  <span class="vertical-align-middle" style="font-size: 1.25rem">Twitch Sign In</span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+    <!-- End Register Page -->
+
+    </div>
+
+    <!-- End Page Scripts -->
+  </section>
+
+  <!-- Terms of Use -->
+  <div class="modal fade modal-size-large" id="termsOfUse" tabindex="-1" aria-hidden="true" style="display: none;">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+          <h4 class="modal-title" id="myModalLabel">Terms of Use</h4>
+        </div>
+        <div class="modal-body" style="font-size: 1.2rem">
+          <div class="row">
+            <div class="col-lg-12">
+              <h4>1. Terms</h4>
+              <p>
+                By accessing this web site, you are agreeing to be bound by these web site Terms and Conditions of Use,
+                all applicable laws and regulations, and agree that you are responsible for compliance with any
+                applicable local laws. If you do not agree with any
+                of these terms, you are prohibited from using or accessing this site. The materials contained in this
+                web site are protected by applicable copyright and trade mark law.
+              </p>
+              <h4>2. Use License</h4>
+              <p>
+                Permission is granted to temporarily download one copy of the materials (information or software) on
+                Apitome Inc's web site for personal, non-commercial transitory viewing only. This is the grant of a
+                license, not a transfer of title, and under this license
+                you may not:
+                <ol>
+                  <li>modify or copy the materials;</li>
+                  <li>use the materials for any commercial purpose, or for any public display (commercial or
+                    non-commercial);</li>
+                  <li>attempt to decompile or reverse engineer any software contained on Apitome Inc's web site;</li>
+                  <li>remove any copyright or other proprietary notations from the materials; or</li>
+                  <li>transfer the materials to another person or "mirror" the materials on any other server.</li>
+                </ol>
+                This license shall automatically terminate if you violate any of these restrictions and may be
+                terminated by Apitome Inc at any time. Upon terminating your viewing of these materials or upon the
+                termination of this license, you must destroy any downloaded
+                materials in your possession whether in electronic or printed format.
+              </p>
+              <h4>3. Disclaimer</h4>
+              <p>
+                The materials on Apitome Inc's web site are provided "as is". Apitome Inc makes no warranties,
+                expressed or implied, and hereby disclaims and negates all other warranties, including without
+                limitation, implied warranties or conditions of merchantability,
+                fitness for a particular purpose, or non-infringement of intellectual property or other violation of
+                rights. Further, Apitome Inc does not warrant or make any representations concerning the accuracy,
+                likely results, or reliability of the
+                use of the materials on its Internet web site or otherwise relating to such materials or on any sites
+                linked to this site.
+              </p>
+              <h4>4. Limitations</h4>
+              <p>
+                In no event shall Apitome Inc or its suppliers be liable for any damages (including, without
+                limitation, damages for loss of data or profit, or due to business interruption,) arising out of the
+                use or inability to use the materials on Apitome Inc's Internet
+                site, even if Apitome Inc or a Apitome Inc authorized representative has been notified orally or in
+                writing of the possibility of such damage. Because some jurisdictions do not allow limitations on
+                implied warranties, or limitations of
+                liability for consequential or incidental damages, these limitations may not apply to you.
+              </p>
+              <h4>5. Revisions and Errata</h4>
+              <p>
+                The materials appearing on Apitome Inc's web site could include technical, typographical, or
+                photographic errors. Apitome Inc does not warrant that any of the materials on its web site are
+                accurate, complete, or current. Apitome Inc may make changes to
+                the materials contained on its web site at any time without notice. Apitome Inc does not, however, make
+                any commitment to update the materials.
+              </p>
+              <h4>6. Links</h4>
+              <p>
+                Apitome Inc has not reviewed all of the sites linked to its Internet web site and is not responsible
+                for the contents of any such linked site. The inclusion of any link does not imply endorsement by
+                Apitome Inc of the site. Use of any such linked web
+                site is at the user's own risk.
+              </p>
+              <h4>7. Site Terms of Use Modifications</h4>
+              <p>
+                Apitome Inc may revise these terms of use for its web site at any time without notice. By using this
+                web site you are agreeing to be bound by the then current version of these Terms and Conditions of Use.
+              </p>
+              <h4>8. Governing Law</h4>
+              <p>
+                Any claim relating to Apitome Inc's web site shall be governed by the laws of the State of Ontario
+                without regard to its conflict of law provisions.
+              </p>
+              <p>
+                General Terms and Conditions applicable to Use of a Web Site.
+              </p>
+              <h4>Privacy Policy</h4>
+              <p>
+                Your privacy is very important to us. Accordingly, we have developed this Policy in order for you to
+                understand how we collect, use, communicate and disclose and make use of personal information. The
+                following outlines our privacy policy.
+                <ul>
+                  <li>Before or at the time of collecting personal information, we will identify the purposes for which
+                    information is being collected.</li>
+                  <li>We will collect and use of personal information solely with the objective of fulfilling those
+                    purposes specified by us and for other compatible purposes, unless we obtain the consent of the
+                    individual concerned or as required by law.</li>
+                  <li>We will only retain personal information as long as necessary for the fulfillment of those
+                    purposes.</li>
+                  <li>We will collect personal information by lawful and fair means and, where appropriate, with the
+                    knowledge or consent of the individual concerned.</li>
+                  <li>Personal data should be relevant to the purposes for which it is to be used, and, to the extent
+                    necessary for those purposes, should be accurate, complete, and up-to-date.</li>
+                  <li>We will protect personal information by reasonable security safeguards against loss or theft, as
+                    well as unauthorized access, disclosure, copying, use or modification.</li>
+                  <li>We will make readily available to customers information about our policies and practices relating
+                    to the management of personal information.</li>
+                  <li>We are committed to conducting our business in accordance with these principles in order to
+                    ensure that the confidentiality of personal information is protected and maintained.</li>
+                </ul>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Video -->
+  <div class="modal fade modal-size-large" id="promoVideo" tabindex="-1" aria-hidden="true" style="display: none;">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content promo-video-modal">
+        <div class="modal-header promo-video-modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close" style="font-size: 3rem; color: white; opacity: .9">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div class="modal-body" style="font-size: 1.2rem">
+          <div class="row margin-bottom-50">
+            <div class="col-lg-12">
+              <iframe class="center-block" width="560" height="315" src="https://www.youtube.com/embed/-_Y2qXTx5o4"
+                frameborder="0" allowfullscreen></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</body>
+
+</html>
