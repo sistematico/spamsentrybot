0c7e298d16de8bb7acd18b30736d0198abac0eab
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-02-22 17:14:02 -0400

    Autocommit por GitHub: alterações: add 'public/bot.php'

--- public/bot.php
+++ public/bot.php
@@ -127,8 +127,9 @@ function processMessage($message)
                 //apiRequest('sendSticker', array('chat_id' => $chat_id, 'sticker' => 'CAACAgEAAxkBAAEB371gJb_kkLwJ8bU0Z2_MM41hn8ZRsQACPAADnjOcH14Lzxv4uFR0HgQ'));
                 break;
 
-
-
+            case (strpos($text, '/ping') === 0):
+                apiRequest('sendMessage', array('chat_id' => $chat_id, 'text' => "\u{1F64C}"));            
+            break;
             case (strpos($text, '/lol') === 0):
                 apiRequest('sendVideo', array('chat_id' => $chat_id, 'video' => BOT_URL . 'vid/no.mp4'));
                 break;
