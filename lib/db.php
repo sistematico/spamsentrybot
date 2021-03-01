03ffb06cf1d6461afac0b5ef925c14ab56d22351
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-02-28 20:20:44 -0400

    Agora com o Laravel

--- lib/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-try {
-    $file_db = new \PDO("sqlite:../db/banco.db");
- } catch (\PDOException $e) {
-    // handle the exception here
- }
--- public/index.php
+++ public/index.php
@@ -1,77 +1,55 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="description" content="">
-  <meta name="author" content="">
-  <title>SpamSentryBot</title>
-  <link rel="shortcut icon" type="image/png" href="assets/img/favicon.ico" />
-  <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.4/css/bootstrap-select.css">
-  <link rel="stylesheet" href="assets/css/landing-page.css">
-  <link rel="stylesheet" type="text/css" href="assets/css/main.css">
-  <script src="assets/js/jquery.min.js"></script>
-  <script src="assets/js/tether.js"></script>
-  <script src="assets/js/bootstrap.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.4/js/bootstrap-select.min.js"></script>
-  <script src="assets/js/main.js"></script>
-</head>
-<body class="theme-default landing-page-body">
-  <section class="page-content">
-    <div class="page-content-inner">
-      <!-- Register Page -->
-      <div class="single-page-block-header">
-        <div class="row">
-          <div class="col-md-4 col-lg-3">
-            <div class="logo">
-              <a href="javascript: history.back();">
-                <img src="assets/img/web-logo-inverse-02.svg" alt="SpamSentryBot" />
-              </a>
-            </div>
-          </div>
-          <div class="col-md-5 col-lg-4 col-md-offset-3 col-lg-offset-5 col-sm-12">
-            <div class="single-page-block-header-menu margin-top-15">
-              <ul class="list-unstyled list-inline height-50 social-flexbox">
-                <li><a href="#" target="_blank"><i class="icmn-youtube"></i></a></li>
-                <li><a href="#"><i class="icon-discord"></i></a></li>
-                <li><a href="#"><i class="icmn-twitter"></i></a></li>
-                <div class="line">
-                </div>
-                <div id="termsofuse" class="height-50 vertical-align">
-                  <a class="vertical-align" style="font-size: 0.9rem" href="#" data-toggle="modal" data-target="#termsOfUse" aria-hidden="true">Painel</a>
-                </div>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="landing-page-block">
-        <div class="row">
-          <div class="col-lg-12 text-center">
-            <img class="center-block landing-page-image" src="assets/img/kappamon_graphic_transparent.svg" />
-            <h1 class="landing-page-header">SpamSentryBot</h1>
-            <h5 class="landing-page-subtext">Um bot altamente configurável para proteger seus grupos no Telegram!</h5>
-            <div class="row">
-              <div class="col-lg-12 text-center">
-                <button class="btn btn-icon btn-danger vertical-align btn-flex margin-bottom-10" data-toggle="modal"
-                  data-target="#promoVideo" aria-hidden="true">
-                  <i class="icmn-youtube margin-right-10 vertical-align-middle" style="font-size: 1.5rem"></i>
-                  <span class="vertical-align-middle" style="font-size: 1.25rem">Watch Video</span>
-                </button>
-                <a href="/login/twitch" class="btn btn-icon btn-purple vertical-align btn-flex margin-bottom-10">
-                  <i class="icmn-twitch margin-right-10 vertical-align-middle" style="font-size: 1.5rem"></i>
-                  <span class="vertical-align-middle" style="font-size: 1.25rem">Twitch Sign In</span>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
+<?php
 
-      </div>
-    </div>
-    </div>
-  </section>
-</body>
-</html>
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+/*
+|--------------------------------------------------------------------------
+| Check If Application Is Under Maintenance
+|--------------------------------------------------------------------------
+|
+| If the application is maintenance / demo mode via the "down" command we
+| will require this file so that any prerendered template can be shown
+| instead of starting the framework, which could cause an exception.
+|
+*/
+
+if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
+    require __DIR__.'/../storage/framework/maintenance.php';
+}
+
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| this application. We just need to utilize it! We'll simply require it
+| into the script here so we don't need to manually load our classes.
+|
+*/
+
+require __DIR__.'/../vendor/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request using
+| the application's HTTP kernel. Then, we will send the response back
+| to this client's browser, allowing them to enjoy our application.
+|
+*/
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$kernel = $app->make(Kernel::class);
+
+$response = tap($kernel->handle(
+    $request = Request::capture()
+))->send();
+
+$kernel->terminate($request, $response);
