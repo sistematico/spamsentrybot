d253de6fcfac9bdd107482f0cc9e083c30dd62cb
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-03-08 22:07:15 -0400

    Updates

--- lib/bl.php
+++ lib/bl.php
@@ -1,10 +1,11 @@
 <?php
 
-function blacklistAdd($file_db) {
+function blacklistAdd($database) {
     $group = '-1001189033145';
     $word = 'asgdfsfs';
-    $sql = 'INSERT OR IGNORE INTO blacklist (group, word, level) values ((SELECT group FROM blacklist WHERE group = :group), :word, 1);';
-    $file_db->prepare($sql);
-    $file_db->execute([':group' => $group, ':word' => $word]);
-    return $file_db->lastInsertId();
+    // $sql = 'INSERT OR IGNORE INTO blacklist (group, word, level) values ((SELECT group FROM blacklist WHERE group = :group), :word, 1);';
+    $sql = 'INSERT INTO blacklist (group, word, level) values (:group, :word, 1);';
+    $database->prepare($sql);
+    $database->execute([':group' => $group, ':word' => $word]);
+    return $database->lastInsertId();
 }
