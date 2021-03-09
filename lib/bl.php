3e1201f2022461f8d7a367477190d779f07df52b
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-03-08 22:16:04 -0400

    Updates

--- lib/bl.php
+++ lib/bl.php
@@ -2,6 +2,8 @@
 
 function blacklistAdd($database) {
     $group = '-1001189033145';
+    //$group = preg_replace( '/[^0-9]/', '', $group );
+    $group = preg_replace("/\D+/", "", $group);
     $word = 'asgdfsfs';
     // $sql = 'INSERT OR IGNORE INTO blacklist (group, word, level) values ((SELECT group FROM blacklist WHERE group = :group), :word, 1);';
     $sql = 'INSERT INTO blacklist (group, word, level) values (:group, :word, 1);';
