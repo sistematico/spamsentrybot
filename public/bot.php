9a18b46d7c7532de9a1006fa188241723656ee3c
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-03-08 21:14:42 -0400

    Updates

--- public/bot.php
+++ public/bot.php
@@ -26,23 +26,17 @@ require_once '../lib/db.php';
 function filterMessage(string $message):bool
 {
     $blacklist = explode(PHP_EOL, file_get_contents(BLACKLIST));
-    foreach($blacklist as $text) {
-        if (stripos(strtolower($message),$text) !== false) return true;
+    foreach($blacklist as $bl) {
+        if (stripos(strtolower($message),$bl) !== false) return true;
     }
     return false;
 }
 
 function processDelete($message_id, $chat_id, $reply_id, $isAdmin = false)
 {
-    if ($isAdmin) {
-        if ($reply_id) {
-            apiRequest("deleteMessage", array('chat_id' => $chat_id, "message_id" => $reply_id));
-            apiRequest("deleteMessage", array('chat_id' => $chat_id, "message_id" => $message_id));
-        } else {
-            apiRequest("sendMessage", array('chat_id' => $chat_id, "reply_to_message_id" => $message_id, "text" => "Uso incorreto, responda a mensagem que deseja apagar com a palavra /del"));
-        }
-    } else {
-        apiRequest("sendMessage", array('chat_id' => $chat_id, "reply_to_message_id" => $message_id, "text" => "Comando somente para admins."));
+    if ($isAdmin && $reply_id) {
+        apiRequest("deleteMessage", array('chat_id' => $chat_id, "message_id" => $reply_id));
+        apiRequest("deleteMessage", array('chat_id' => $chat_id, "message_id" => $message_id));
     }
 }
 
@@ -62,8 +56,8 @@ function processMessage($message)
     $reply_id = $message['reply_to_message']['message_id'] ?? false;
     $user_id = $message['from']['id'];
 
-    $getChat = apiRequest("getChat", array('chat_id' => $chat_id));
-    $groupName = basename($getChat['invite_link']);
+    //$getChat = apiRequest("getChat", array('chat_id' => $chat_id));
+    //$groupName = basename($getChat['invite_link']);
 
     $member = apiRequest("getChatMember", array('chat_id' => $chat_id, "user_id" => $user_id));
     $isAdmin = $member['status'] === 'creator' || $member['status'] === 'administrator';
@@ -71,73 +65,39 @@ function processMessage($message)
     $originalUsername = (isset($message['reply_to_message']['from']['username']) ? $message['reply_to_message']['from']['username'] : $message['reply_to_message']['from']['first_name'] . ' ' . $message['reply_to_message']['from']['last_name']);
 
     if (isset($update['callback_query']) && isset($callback_id)) {
-        apiRequest("answerCallbackQuery", array('callback_id' => $callback_id, 'text' => 'Uhullll {$callback_user}', 'show_alert' => true));
+        apiRequest("answerCallbackQuery", array('callback_id' => $callback_id, 'text' => 'Uhullll', 'show_alert' => true));
     }
 
     if (isset($message['text'])) {
         $text = $message['text'];
 
         switch ($text) {
-            case (strpos($text, '/debug') === 0):
-                if ($isAdmin) {
-                    apiRequest("sendChatAction", array('chat_id' => $chat_id, 'action' => 'typing'));
-
-                    $msg = "Chat ID: {$chat_id}\n";
-                    $msg .= "Message ID: {$message_id}\n";
-                    $msg .= "User ID: {$user_id}\n";
-                    $msg .= "Reply ID: {$reply_id}\n";
-                    $msg .= "Username: {$username}\n";
-                    $msg .= "Original Username: {$originalUsername}\n";
-                    $msg .= "Member Status: " . $member['status'] . "\n";
-                    $msg .= "Member Is Bot: " . $member['is_bot'] . "\n";
-                    $msg .= "Member First: " . $member['first_name'] . "\n";
-                    $msg .= "Member User: " . $member['username'] . "\n";
-                    $msg .= "Member Arr1: " . implode(',', $member[0]) . "\n";
-                    $msg .= "Member Arr2: " . implode(',', $member[1]) . "\n";
-                    $msg .= "URL: " . BOT_URL;
-                    apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => $msg));
-                }
-                break;
             case (strpos($text, '/del') === 0):
                 apiRequest("sendChatAction", array('chat_id' => $chat_id, 'action' => 'typing'));
                 processDelete($message_id, $chat_id, $reply_id, $isAdmin);
                 break;
+
             case (strpos($text, '/warn') === 0):
                 apiRequest("sendChatAction", array('chat_id' => $chat_id, 'action' => 'typing'));
                 warnUser($message_id, $chat_id, $reply_id, $isAdmin);
                 break;
-            case (strpos($text, '/logs') === 0):
-                apiRequest("sendChatAction", array('chat_id' => $chat_id, 'action' => 'typing'));
-                $log = readLog();
-                apiRequest('sendMessage', array('chat_id' => $chat_id, 'text' => "Logs: {$log['log']}\n\n{$log['linhas']} linhas."));
-                break;
+
             case (strpos($text, '/ban') === 0):
                 apiRequest("sendChatAction", array('chat_id' => $chat_id, 'action' => 'typing'));
-                //apiRequest("sendVideo", array('chat_id' => $chat_id, "reply_to_message_id" => $message_id, "video" => CABRON_URL . 'vid/fogo.mp4'));
                 apiRequest('sendSticker', array('chat_id' => $chat_id, 'sticker' => 'CAACAgEAAxkBAAEB371gJb_kkLwJ8bU0Z2_MM41hn8ZRsQACPAADnjOcH14Lzxv4uFR0HgQ'));
                 break;
-            case (strpos($text, '/groupname') === 0):
-                apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => $groupName));
+
+            case (strpos($text, '/blacklist') === 0):
+                apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => BLACKLIST));
                 break;
-            case (strpos($text, '/getchat') === 0):
-                apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => implode(',', array_keys($member))));
-                break;
-            case (strpos($text, '/getcv') === 0):
-                apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => implode(' - ', $member)));
-                break;
+
             case (strpos($text, '/gline') === 0):
                 apiRequestJson("sendMessage", array('chat_id' => $chat_id, "text" => 'BAN Global?', 'reply_markup' => array(
                     'keyboard' => array(array('Sim', 'Não')),
                     'one_time_keyboard' => true,
                     'resize_keyboard' => true)));
-
-                //apiRequest("sendChatAction", array('chat_id' => $chat_id, 'action' => 'typing'));
-                //apiRequest('sendSticker', array('chat_id' => $chat_id, 'sticker' => 'CAACAgEAAxkBAAEB371gJb_kkLwJ8bU0Z2_MM41hn8ZRsQACPAADnjOcH14Lzxv4uFR0HgQ'));
                 break;
-
             case (strpos($text, '/kline') === 0):
-                //apiRequestJson("sendMessage", array('chat_id' => $chat_id, "text" => 'BAN Global?', 'reply_markup' => array('inline_keyboard' => array(array('Sim', 'Não')))));
-
                 apiRequestJson(
                     "sendMessage", 
                     array(
@@ -147,26 +107,14 @@ function processMessage($message)
                             'inline_keyboard' => array(
                                 array(
                                     array('text'=>'Sim','callback_data'=>'sim'),
-                                    array('text'=>'Sim','callback_data'=>'nao')
+                                    array('text'=>'Não','callback_data'=>'nao')
                                 )
-                                // array(
-                                    // array('text'=>'Lotofácil','url'=>'http://g1.globo.com/loterias/lotofacil.html'),
-                                    // array('text'=>'Lotomania','url'=>'http://g1.globo.com/loterias/lotomania.html')
-                                // )
                             ),
                             'one_time_keyboard' => true,
                             'selective' => true
                         )
                     )
                 );
-                break;
-            case (strpos($text, '/ping') === 0):
-                if (!$isAdmin)
-                    apiRequest('sendMessage', array('chat_id' => $chat_id, 'text' => "\u{1F64C}"));
-            break;
-            case (strpos($text, '/lol') === 0):
-                $fp = new CURLFile(realpath(VIDEOS . 'no.mp4'));
-                apiRequestFile('sendVideo', array('chat_id' => $chat_id, 'video' => $fp));
                 break;
             case (strpos($text, '/pc') === 0):
                 $fp = new CURLFile(realpath(VIDEOS . 'pc.mp4'));
@@ -175,8 +123,6 @@ function processMessage($message)
             default:
                 if (filterMessage($text)) {
                     apiRequest("deleteMessage", array('chat_id' => $chat_id, "message_id" => $message_id));
-                    //apiRequest("sendChatAction", array('chat_id' => $chat_id, 'action' => 'typing'));
-                    //apiRequest('sendMessage', array('chat_id' => $chat_id, 'text' => "\u{1F621} @{$username}"));
                 }
                 break;
         }
